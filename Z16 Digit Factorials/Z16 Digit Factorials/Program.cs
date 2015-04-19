317b10884548f6fb2410e399f0e294690de11a12
jdi4 <jzxp.a4t@gmail.com>
2015-04-19 20:20:29 +0200

    Dodanie kodu rozwiązania zadania

--- Z16 Digit Factorials/Z16 Digit Factorials/Program.cs
+++ Z16 Digit Factorials/Z16 Digit Factorials/Program.cs
@@ -11,6 +11,8 @@ namespace Z16_Digit_Factorials
         static void Main(string[] args)
         {
             int[] calculated_factorials = new int[10];
+            const int upper_bound = 1854721; // wiki
+            int sum_to_find = 0;
 
             for (int i = 0 ; i < 10 ; i++)
             {
@@ -23,6 +25,29 @@ namespace Z16_Digit_Factorials
             //{
             //    Console.WriteLine("{0}! = {1}", (j++).ToString(), i.ToString());
             //}
+
+            for (int i = 10; i <= upper_bound; i++)
+            {
+                int part = i;
+                int digit; // = part % 10;
+                int sum = 0;
+
+                while (part > 0)
+                {
+                    digit = part % 10;
+                    sum += calculated_factorials[digit];
+                    part /= 10;
+                }
+
+                if (sum == i)
+                {
+                    sum_to_find += i;
+                }
+
+            }
+
+            Console.WriteLine("Suma znalezionych liczb: {0}", sum_to_find.ToString());
+
 
             Console.ReadKey();
         }
